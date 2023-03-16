ad7abfffbb4fb075e69e8d82ae5922903796b08f
Shun Usami <usatie@gmail.com>
2023-03-16 15:31:26 +0900

    Modify Analyzer to return Diagnostic instead of report error

--- convention.go
+++ convention.go
@@ -1,13 +1,12 @@
 package convention
 
 import (
+	"fmt"
 	"go/ast"
 	"go/token"
 	"go/types"
 
 	"golang.org/x/tools/go/analysis"
-	"golang.org/x/tools/go/analysis/passes/inspect"
-	"golang.org/x/tools/go/ast/inspector"
 )
 
 const doc = "convention detects code that goes against conventions and prompting the writing of comments for such code."
@@ -17,26 +16,27 @@ var Analyzer = &analysis.Analyzer{
 	Name: "convention",
 	Doc:  doc,
 	Run:  run,
-	Requires: []*analysis.Analyzer{
-		inspect.Analyzer,
-	},
 }
 
 func run(pass *analysis.Pass) (any, error) {
-	inspect := pass.ResultOf[inspect.Analyzer].(*inspector.Inspector)
-
-	nodeFilter := []ast.Node{
-		(*ast.IfStmt)(nil),
+	var result []analysis.Diagnostic
+	for _, file := range pass.Files {
+		cmap := ast.NewCommentMap(pass.Fset, file, file.Comments)
+		ast.Inspect(file, func(n ast.Node) bool {
+			if n == nil {
+				return false
+			}
+			if n, ok := n.(*ast.IfStmt); ok {
+				d, found := checkIfCond(pass, n, cmap)
+				if found {
+					result = append(result, d)
+				}
+			}
+			return true
+		})
 	}
 
-	inspect.Preorder(nodeFilter, func(n ast.Node) {
-		switch n := n.(type) {
-		case *ast.IfStmt:
-			checkIfCond(pass, n.Cond)
-		}
-	})
-
-	return nil, nil
+	return result, nil
 }
 
 func isNil(pass *analysis.Pass, n ast.Node) bool {
@@ -49,8 +49,8 @@ func isNil(pass *analysis.Pass, n ast.Node) bool {
 	return ok
 }
 
-func checkIfCond(pass *analysis.Pass, cond ast.Expr) {
-	ast.Inspect(cond, func(n ast.Node) bool {
+func checkIfCond(pass *analysis.Pass, ifStmt *ast.IfStmt, cmap ast.CommentMap) (d analysis.Diagnostic, found bool) {
+	ast.Inspect(ifStmt.Cond, func(n ast.Node) bool {
 		if n == nil {
 			return false
 		}
@@ -58,11 +58,11 @@ func checkIfCond(pass *analysis.Pass, cond ast.Expr) {
 		if !ok {
 			return true
 		}
-		// Only check ==
+		// operator is ==
 		if binOp.Op != token.EQL {
 			return true
 		}
-		// x or y should be nil
+		// x or y is nil
 		var t types.Type
 		if isNil(pass, binOp.X) {
 			t = pass.TypesInfo.TypeOf(binOp.Y)
@@ -74,24 +74,23 @@ func checkIfCond(pass *analysis.Pass, cond ast.Expr) {
 		if t == nil {
 			return true
 		}
-		// x or should implements error interface
+		// x or y implements error interface
 		errType := types.Universe.Lookup("error").Type()
 		if !types.Implements(t, errType.Underlying().(*types.Interface)) {
 			return true
 		}
-		/* This is not working
-		// should have comment
-		for _, file := range pass.Files {
-			cmap := ast.NewCommentMap(pass.Fset, file, file.Comments)
-			cmt, ok := cmap[n]
-			if ok {
-				// found comment
-				fmt.Println(cmt)
-				return true
+		// There is no comments about breaking convention
+		for _, cg := range cmap {
+			for _, comment := range cg {
+				if pass.Fset.Position(comment.Pos()).Line == pass.Fset.Position(ifStmt.Pos()).Line {
+					return true
+				}
 			}
 		}
-		*/
-		pass.Reportf(n.Pos(), "Convention is handling error case by if statement. If not you should comment.")
+		found = true
+		d = analysis.Diagnostic{Pos: ifStmt.Pos(), Message: "warning: Handle error case or leave comment."}
+		fmt.Println(pass.Fset.Position(ifStmt.Pos()), ": warning: Handle error case or leave comment.")
 		return true
 	})
+	return d, found
 }
