9d055675d4c1fbfc9c6e1e3b4f61dfb97e024371
Shun Usami <usatie@gmail.com>
2023-03-16 18:05:36 +0900

    :construction: WIP: get package and analyze each package

--- scrape/main.go
+++ scrape/main.go
@@ -35,6 +35,33 @@ func getPackages() ([]Package, error) {
 	return packages, nil
 }
 
+func getPackage(pkg Package) {
+	// exec packageでgo getコマンドを叩く
+	// まずは手でそのコマンドを叩いてみる
+	// 1. Create temp directory
+	// 2. Cd to temp directory ( exec packageでcdできる）
+	// 3. go mod initで初期化（最後の引数は適当）
+	// 4. indexをparseしたpackage名を使ってgo get {module_path}/...
+	// 5. go vet -vettool {module_path}/...
+	/*
+		// Get response
+		path := fmt.Sprintf("https://proxy.golang.org/%s/@v/%s.info", pkg.Path, pkg.Version)
+		resp, err := http.Get(path)
+		if err != nil {
+			return nil, err
+		}
+		defer resp.Body.Close()
+
+		// Scan response line by line
+		scanner := bufio.NewScanner(resp.Body)
+		for scanner.Scan() {
+			// Decode each line
+			fmt.Println(scanner.Text())
+		}
+		return nil, nil
+	*/
+}
+
 func main() {
 
 	packages, err := getPackages()
@@ -42,4 +69,5 @@ func main() {
 		panic(err)
 	}
 	fmt.Printf("%+v\n", packages[:5])
+	getPackage(packages[0])
 }
