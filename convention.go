1876adeb2b02e2f963e4ffc577e96403f6d34364
Shun Usami <usatie@gmail.com>
2023-03-16 14:57:36 +0900

    Detect `if err == nil`

--- convention.go
+++ convention.go
@@ -2,13 +2,15 @@ package convention
 
 import (
 	"go/ast"
+	"go/token"
+	"go/types"
 
 	"golang.org/x/tools/go/analysis"
 	"golang.org/x/tools/go/analysis/passes/inspect"
 	"golang.org/x/tools/go/ast/inspector"
 )
 
-const doc = "convention is ..."
+const doc = "convention detects code that goes against conventions and prompting the writing of comments for such code."
 
 // Analyzer is ...
 var Analyzer = &analysis.Analyzer{
@@ -24,17 +26,72 @@ func run(pass *analysis.Pass) (any, error) {
 	inspect := pass.ResultOf[inspect.Analyzer].(*inspector.Inspector)
 
 	nodeFilter := []ast.Node{
-		(*ast.Ident)(nil),
+		(*ast.IfStmt)(nil),
 	}
 
 	inspect.Preorder(nodeFilter, func(n ast.Node) {
 		switch n := n.(type) {
-		case *ast.Ident:
-			if n.Name == "gopher" {
-				pass.Reportf(n.Pos(), "identifier is gopher")
-			}
+		case *ast.IfStmt:
+			checkIfCond(pass, n.Cond)
 		}
 	})
 
 	return nil, nil
+}
+
+func isNil(pass *analysis.Pass, n ast.Node) bool {
+	ident, ok := n.(*ast.Ident)
+	if !ok {
+		return false
+	}
+	obj := pass.TypesInfo.ObjectOf(ident)
+	_, ok = obj.(*types.Nil)
+	return ok
+}
+
+func checkIfCond(pass *analysis.Pass, cond ast.Expr) {
+	ast.Inspect(cond, func(n ast.Node) bool {
+		if n == nil {
+			return false
+		}
+		binOp, ok := n.(*ast.BinaryExpr)
+		if !ok {
+			return true
+		}
+		// Only check ==
+		if binOp.Op != token.EQL {
+			return true
+		}
+		// x or y should be nil
+		var t types.Type
+		if isNil(pass, binOp.X) {
+			t = pass.TypesInfo.TypeOf(binOp.Y)
+		} else if isNil(pass, binOp.Y) {
+			t = pass.TypesInfo.TypeOf(binOp.X)
+		} else {
+			return true
+		}
+		if t == nil {
+			return true
+		}
+		// x or should implements error interface
+		errType := types.Universe.Lookup("error").Type()
+		if !types.Implements(t, errType.Underlying().(*types.Interface)) {
+			return true
+		}
+		/* This is not working
+		// should have comment
+		for _, file := range pass.Files {
+			cmap := ast.NewCommentMap(pass.Fset, file, file.Comments)
+			cmt, ok := cmap[n]
+			if ok {
+				// found comment
+				fmt.Println(cmt)
+				return true
+			}
+		}
+		*/
+		pass.Reportf(n.Pos(), "Convention is handling error case by if statement. If not you should comment.")
+		return true
+	})
 }
--- testdata/src/a/a.go
+++ testdata/src/a/a.go
@@ -1,7 +1,31 @@
 package a
 
+func doSomething() error {
+	return nil
+}
+
+func makePointer() *struct{} {
+	return nil
+}
+
 func f() {
-	// The pattern can be written in regular expression.
-	var gopher int // want "pattern"
-	print(gopher)  // want "identifier is gopher"
+	// Good:
+	if err := doSomething(); err != nil {
+		return
+	}
+
+	// Good:
+	if err := doSomething(); err == nil { // if NO error
+		return
+	}
+
+	// Bad: missing comment
+	if err := doSomething(); err == nil {
+		return
+	}
+
+	// Good: It's not error type
+	if err := makePointer(); err == nil {
+		return
+	}
 }
