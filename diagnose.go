60de68957b769939f234ffb5d3065320072ba2e2
Shun Usami <usatie@gmail.com>
2023-03-17 16:14:15 +0900

    Refactor diagnose.go

--- diagnose.go
+++ diagnose.go
@@ -6,19 +6,20 @@ import (
 	"golang.org/x/tools/go/analysis"
 )
 
+// Add more diagnosers here
+var diagnosers = []*analysis.Analyzer{
+	ErrorHandlingDiagnoser,
+}
+
 var DiagnoseAnalyzer = &analysis.Analyzer{
 	Name:       "diagnose",
 	Doc:        "diagnose only. It doesn't report.",
 	Run:        diagnose,
 	ResultType: reflect.TypeOf([]analysis.Diagnostic{}),
-	Requires:   []*analysis.Analyzer{ErrorHandlingDiagnoser},
+	Requires:   diagnosers,
 }
 
 func diagnose(pass *analysis.Pass) (any, error) {
-	diagnosers := []*analysis.Analyzer{
-		ErrorHandlingDiagnoser,
-	}
-
 	var combined []analysis.Diagnostic
 	for _, diagnoser := range diagnosers {
 		if result, ok := pass.ResultOf[diagnoser].([]analysis.Diagnostic); ok {
